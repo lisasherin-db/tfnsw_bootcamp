02b8bcdf9e7fdc9b203194f91b118abdbdd67b92
yasmk <yasmk>
2023-07-21 06:42:42 +0000

    instruct

--- DE-Batch/api-bronze.py
+++ DE-Batch/api-bronze.py
@@ -4,49 +4,48 @@
 
 # COMMAND ----------
 
-current_user_id = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-datasets_location = f'/FileStore/tmp/{current_user_id}/datasets/'
+# MAGIC %run ../utils/setup
 
-catalog_name = "transport_bootcamp"
-database_name = current_user_id.split('@')[0].replace('.','_')+'_bootcamp'
+# COMMAND ----------
 
 
 dbutils.fs.rm(datasets_location, True)
 
-
 spark.sql(f'CREATE CATALOG IF NOT EXISTS {catalog_name}')
-spark.sql(f'create database if not exists {catalog_name}.{database_name};')
-spark.sql(f'use {catalog_name}.{database_name}')
+spark.sql(f'create database if not exists {database};')
+spark.sql(f'use {database}')
 
-print (f"Created database :  {catalog_name}.{database_name}") 
+print (f"Created database :  {database}") 
+print(f"bronze table name: {bronze_table_name}")
 
 # COMMAND ----------
 
 import time
-import request
+import requests
 from datetime import datetime
 
 # COMMAND ----------
 
 def get_sydney_trains_data():
     # API endpoint URL
-    url = 'https://api.transport.nsw.gov.au/v2/gtfs/vehiclepos/sydneytrains'
+    url = api_uri
 
     # Set the required headers
     headers = {
-        'Authorization': f'apikey {dbutils.secrets.get(scope="lisasherin", key="opendata_apikey")}',
+        'Authorization': f'apikey {dbutils.secrets.get(scope=scope_name, key="opendata_apikey")}',
         'Accept': 'application/x-google-protobuf'
     }
 
     # Make the API call
-    response = requests.get(url, headers=headers)
+    data = None
+    try:
+      response = requests.get(url, headers=headers)
 
-    response.raise_for_status() # this will raise an exception if using dataflow we can retry the api call (needs some error handling)
- 
-    # # Check if the request was successful (status code 200)
-    # if response.status_code == 200:
-    #     # Retrieve the data from the response
-    data = response.content
+      response.raise_for_status() # this will raise an exception if using dataflow we can retry the api call 
+
+      data = response.content
+    except Exception as e:
+      rasie (e)
 
     return data
 
@@ -60,11 +59,14 @@ sleep_time = 10
 api_data = get_sydney_trains_data()
 data = [{
   "source": "api_transport_nsw",
-  "timestamp": datetime.utcnow(),
+  "timestamp": datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%S"),
   "data": api_data,
 }]
+
 df = spark.createDataFrame(data=data)
-df.write.mode('append').option("mergeSchema", "true").saveAsTable(f"{catalog_name}.{database_name}.bronze_train_data")
+
+#write the data into a delta table (delta is the defaul format)
+df.write.mode('append').option("mergeSchema", "true").saveAsTable(bronze_table_name)
 # time.sleep(sleep_time)
 
 
@@ -75,6 +77,11 @@ df.write.mode('append').option("mergeSchema", "true").saveAsTable(f"{catalog_nam
 # MAGIC - in python you can use display()
 # MAGIC - you can switch to SQL by adding %sql 
 # MAGIC - to read a delta table into a dataframe you can use this syntax: spark.read.table(table_name)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC delete  from transport_bootcamp.yas_mokri_bootcamp.bronze_train_data
 
 # COMMAND ----------
 
--- DE-Batch/train_data_silver.py
+++ DE-Batch/train_data_silver.py
@@ -1,28 +1,88 @@
 # Databricks notebook source
-catalog_name = "transport_bootcamp"
-current_user_id = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-database_name = current_user_id.split('@')[0].replace('.','_')+'_bootcamp'
+# MAGIC %run ../utils/setup
 
 # COMMAND ----------
 
-df_bronze = spark.read.table(f"{catalog_name}.{database_name}.bronze_train_data")
+print (f"Created database :  {database}") 
+print(f"bronze table name: {bronze_table_name}")
+print(f"silver table name: {silver_table_name}")
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ## Read protobuf data from the Bronze table
+
+# COMMAND ----------
+
+df_bronze = spark.read.table(bronze_table_name)
 display(df_bronze)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Parse protobuf data using fom_protobuf 
 
 # COMMAND ----------
 
 from pyspark.sql.protobuf.functions import from_protobuf
 from pyspark.sql.functions import col, explode
 
-descriptor_file = "/Workspace/Repos/yas.mokri@databricks.com/tfnsw_bootcamp/gtfs-realtime_1007_extension.desc"
+proto_df = df_bronze.select(col("timestamp").alias("ingest_time") , from_protobuf(df_bronze.data, "FeedMessage", descFilePath=descriptor_file).alias("proto"))
 
-proto_df = (
-    df_bronze.select(
-      from_protobuf(df_bronze.data, "FeedMessage", descFilePath=descriptor_file).alias("proto")
-    )
-)
+display(proto_df)
 
-unpacked_df = proto_df.select('proto.*').select(explode(col('entity')).alias("entity")).select("entity", "entity.*").select("entity", "id", "alert","vehicle.*")#.select("entity", "id", "alert","trip.*", "vehicle.*")
+
+# COMMAND ----------
+
+proto_df.schema
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## unpack struct data into columns 
+
+# COMMAND ----------
+
+unpacked_df = proto_df.select('ingest_time', 'proto.*').select('ingest_time', explode(col('entity')).alias("entity"))
+
+# hand on exercise- continue by unpacking some more fields like entity then vehicle, use a pattern similar proto_df.select('proto.*')
+unpacked_df = unpacked_df.select('ingest_time', "entity", "entity.*").select('ingest_time', "entity", "id", "alert","vehicle.*")
+
 display(unpacked_df)
+
+# COMMAND ----------
+
+# you can do something similar using SQL as well. Then you can use unpacked_df = spark.sql("you_sql_query") to acheive the same results
+#https://docs.databricks.com/optimizations/semi-structured.html
+
+# COMMAND ----------
+
+proto_df.createOrReplaceTempView("proto_temp_view")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select entity.* from (select (explode(proto.entity)) as entity from proto_temp_view)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ## Write to silver
+# MAGIC Write the results to the silver table (you can use silver_table_name) using the method we used earlier to write to Bronze then use a SQL statement to verify the results
+
+# COMMAND ----------
+
+print(silver_table_name)
+
+# COMMAND ----------
+
+unpacked_df.write.mode('append').option("mergeSchema", "true").saveAsTable(silver_table_name)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Alternative method using SQL
 
 # COMMAND ----------
 
@@ -50,5 +110,18 @@ spark.conf.set("spark.databricks.delta.schema.autoMerge.enabled", "true")
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ##MERGE INTO using id assuming it's unique and updating the table two times 
+# MAGIC %md 
+# MAGIC ##DML
+# MAGIC Update congestion_level from UNKNOWN_CONGESTION_LEVEL to another value for one or some of the rows in the table
+# MAGIC Write a SQL query to do this either by using %sql or spark.sql(sql_query)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT id, congestion_level FROM transport_bootcamp.yas_mokri_bootcamp.silver_train_data WHERE id in (1,2,10);
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC UPDATE transport_bootcamp.yas_mokri_bootcamp.silver_train_data SET congestion_level="VERY_KNOWN_CONGESTION_LEVEL" WHERE id in (1,2,10) ;
+# MAGIC SELECT id, congestion_level FROM transport_bootcamp.yas_mokri_bootcamp.silver_train_data WHERE id in (1,2,10);
