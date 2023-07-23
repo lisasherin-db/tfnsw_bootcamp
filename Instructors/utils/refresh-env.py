21f49a086189b433c19dc88202ae05823768c710
lisasherin-db <lisasherin-db>
2023-07-23 04:40:49 +0000

    remove create catalog

--- Instructors/utils/refresh-env.py
+++ Instructors/utils/refresh-env.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 dbutils.fs.rm(datasets_location, True)
 
-spark.sql(f'CREATE CATALOG IF NOT EXISTS {catalog_name}')
+# spark.sql(f'CREATE CATALOG IF NOT EXISTS {catalog_name}')
 spark.sql(f'create database if not exists {database};')
 spark.sql(f'use {database}')
 
